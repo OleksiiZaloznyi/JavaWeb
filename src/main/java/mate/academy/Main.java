4a3e2c1494f9798f67b54d0712b7eb58df10b01a
yuliia.kyianytsia <u.kiyanitsa@gmail.com>
2023-01-31 19:01:21 +0200

    Initial commit

---
+++ src/main/java/mate/academy/Main.java
@@ -0,0 +1,12 @@
+package mate.academy;
+
+public class Main {
+    public static void main(String[] args) {
+        // Display a page in browser
+        // - add required dependency
+        // - create Servlet class
+        // - configure web.xml
+        // - create index.jsp page
+        // - run web app
+    }
+}
